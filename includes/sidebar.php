745adfafc77aa241c93cc058fd7fa3fc67eb0837
Fadodo <fadydrive96@gmail.com>
2019-01-14 21:41:56 -0800

    searhc engine feature

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,53 +1,56 @@
-            <!-- Blog Sidebar Widgets Column -->
+        <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
+
 
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
+                    <form action="search.php" method="post">
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input type="text" name="search" class="form-control">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
+                    </form>
                     <!-- /.input-group -->
                 </div>
 
                 <!-- Blog Categories Well -->
                 <div class="well">
                     <h4>Blog Categories</h4>
+
+                <?php
+
+                $query = "SELECT * FROM categories";
+                $select_cata = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($select_cata)){
+                    $cat_title = $row['cat_title'];
+                ?>
+
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
+
+                    <?php echo "<li><a href='#'>{$cat_title}</a></li>";
+                    ?> 
+
+                </ul>
                 </div>
+                </div>
+                
+
+                    <?php
+
+                        }
+
+                        ?>     
+           
+
+           </div>
 
                 <!-- Side Widget Well -->
                 <div class="well">
@@ -55,6 +58,6 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
                 </div>
 
-            </div>
+        
 
         </div>
